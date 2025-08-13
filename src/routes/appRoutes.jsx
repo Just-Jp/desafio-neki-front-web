9eac9a5031b290d6c84a633acd4b7ecc2ead981b
João Pedro B. Dias <jotape_dias@outlook.com>
2025-08-13 13:37:05 -0300

    adicionando proteção de rotas

--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -2,13 +2,18 @@ import { Route, Routes } from 'react-router-dom'
 import LoginPage from '../pages/login/login'
 import RegisterPage from '../pages/register/register'
 import HomePage from '../pages/home/home'
+import { ProtectedRoute } from './ProtectedRoute';
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path='/' element={<LoginPage/>}></Route>
-            <Route path='/register' element={<RegisterPage/>}></Route>
-            <Route path='/home' element={<HomePage/>}></Route>        
+            <Route path='/' element={<LoginPage />}></Route>
+            <Route path='/register' element={<RegisterPage />}></Route>
+            <Route path='/home' element={
+                <ProtectedRoute>
+                    <HomePage />
+                </ProtectedRoute>
+            }></Route>
         </Routes>
     )
 }
